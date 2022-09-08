701383de51b815c722df2fb876ccc4ceea2775ab
euNung <pandamon24@gmail.com>
2022-09-08 15:27:41 +0900

    commit before checkout

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path');
 const HtmlwebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     devtool: "source-map",
-    // mode: "development",
+    mode: "development",
     entry: "./js/index.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
-        // publicPath: "./dist",
+        publicPath: "./dist",
         clean: true
     },
-    // plugins: [
-    //     new HtmlwebpackPlugin(),
-    // ],
-    // devServer: {
-    //     devMiddleware: { publicPath: "/dist" },
-    //     static: { directory: path.join(__dirname) },
-    //     hot: true,
-    // }
+    plugins: [
+        new HtmlwebpackPlugin(),
+    ],
+    devServer: {
+        devMiddleware: { publicPath: "/dist" },
+        static: { directory: path.join(__dirname) },
+        hot: true,
+    }
 }
